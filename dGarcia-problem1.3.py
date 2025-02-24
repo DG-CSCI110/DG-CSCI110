c135e867092d7c330c35e3eab2c63cdcf59a8b0f
Dylan Garcia <xdg222x@gmail.com>
2025-02-23 20:35:49 -0700

    Week 3
    Exercise 2.14 Problem 1

---
+++ dGarcia-problem1.3.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        Chapter 2
+# Purpose:     Section 2.14 Problem 1
+#
+# Author:      DylanG
+#
+# Created:     02/23/2025
+# Copyright:   (c) DylanG 2025
+# Licence:     <my licence>
+#-------------------------------------------------------------------------------
+
+one = "All "
+two = "work "
+three = "and "
+four = "no "
+five = "play "
+six = "makes "
+seven = "Jack "
+eight = "a "
+nine = "dull "
+ten = "boy."
+print(one + two + three + four + five + six + seven + eight + nine + ten)
